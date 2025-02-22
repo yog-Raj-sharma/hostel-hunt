386aca707e6ebeff8cee3d1b2d94640bf4c5aaaa
yog-Raj-sharma <103878161+yog-Raj-sharma@users.noreply.github.com>
2025-02-22 22:27:47 +0530

    Database connection

--- backend/db.js
+++ backend/db.js
@@ -3,8 +3,8 @@ require('dotenv').config();
 
 const dbPassword = process.env.DB_PASSWORD;
 
-//mongoose.connect(`mongodb+srv://yograjsharma:${dbPassword}@cluster0.7o0rg75.mongodb.net/hostel-hunt`, {
-mongoose.connect('mongodb+srv://hostel-hunt:${dbPassword}@cluster-1.j4wqe.mongodb.net/hostel-hunt',{ 
+mongoose.connect(`mongodb+srv://yograjsharma:${dbPassword}@cluster-1.j4wqe.mongodb.net/hostel-hunt`, {
+//mongoose.connect('mongodb+srv://hostel-hunt:${dbPassword}@cluster-1.j4wqe.mongodb.net/hostel-hunt',{ 
 })
   .then(() => {
     console.log('Connected to MongoDB');
